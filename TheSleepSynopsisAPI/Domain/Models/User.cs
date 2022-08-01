f5d4cad3985cdcde216f687ae2b03f817a42934c
Tom K <tomknighton@icloud.com>
2022-08-01 01:01:07 +0100

    feat(domain): Add profile picture url to user model

--- TheSleepSynopsisAPI/Domain/Models/User.cs
+++ TheSleepSynopsisAPI/Domain/Models/User.cs
@@ -6,7 +6,10 @@ namespace TheSleepSynopsisAPI.Domain.Models
         public string UserUUID { get; set; }
         public string UserName { get; set; }
         public string UserFullName { get; set; }
+        public string UserProfilePictureUrl { get; set; }
         public UserRole UserRole { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
     }
 
     public enum UserRole
